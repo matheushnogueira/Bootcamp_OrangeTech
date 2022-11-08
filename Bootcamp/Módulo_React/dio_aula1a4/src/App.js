ed503c3817d8b32619dce42c641a25c487bd8a31
mafeushns <matheusnog001@gmail.com>
2022-11-08 15:44:40 -0300

    .

--- Bootcamp/Módulo_React/dio_aula1a4/src/App.js
+++ Bootcamp/Módulo_React/dio_aula1a4/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Home } from './Pages/home'
 import { Login } from './Pages/login'
 import { Feed } from './Pages/feed'
+import { Teste } from './Pages';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
          <Route path="/" element={<Home />}/>
          <Route path="/login" element={<Login />}/>
          <Route path="/feed" element={<Feed />}/>
+         <Route path="/teste" element={<Teste />}/>
       </Routes>
     </Router>
   );
